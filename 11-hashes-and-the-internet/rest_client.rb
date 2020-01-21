b09dcb23992149e103191a19f3864a3b560b671b
othaderek <otha.hernandez@gmail.com>
2020-01-21 14:47:15 -0500

    Final lecture notes

--- 11-hashes-and-the-internet/rest_client.rb
+++ 11-hashes-and-the-internet/rest_client.rb
@@ -2,5 +2,24 @@ require 'rest-client'
 require 'pry'
 require 'json'
 
-binding.pry
+def welcome
+    puts " 📕 Welcome to Book Title Searcher 📕 "
+end
+
+def get_user_input
+    puts "What book do you want to search??"
+    book = gets.chomp
+    do_search(book)
+end
+
+def do_search(book)
+    items_hash = RestClient.get("https://www.googleapis.com/books/v1/volumes?q=#{book}")
+    volume_info = JSON.parse(items_hash)["items"].map { |item| item["volumeInfo"] }
+    puts volume_info.map {|vol| vol["title"]}
+end
+
+welcome
+get_user_input
+
+# binding.pry
 0
