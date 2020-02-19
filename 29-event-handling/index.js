060ecbbe3ee79e02ad446c69d2e414300ad9c7f1
Ian Hollander <ianholla@gmail.com>
2020-02-19 13:32:59 -0500

    before event handling

---
+++ 29-event-handling/index.js
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Morning code
+// /****** DOM Elements ******/
+// const duckList = document.querySelector("#ducks-container")
+
+// /****** Render Helpers ******/
+// function renderDuckListItem(duckObj) {
+//   // 1. Create the DOM node
+//   const duckLi = document.createElement("li")
+
+//   // 2. Add any attributes it needs
+//   duckLi.textContent = duckObj.name
+//   duckLi.classList.add("item")
+
+//   // 3. Append to its parent element
+//   duckList.append(duckLi)
+// }
+
+// function renderDuckList(ducksArray) {
+//   ducksArray.forEach(function (duckObj) {
+//     renderDuckListItem(duckObj)
+//   })
+// }
+
+// /****** Initial Render ******/
+// renderDuckList(ducksArray)
---
+++ 29-event-handling/pTagChallenge.js
@@ -0,0 +1,3 @@
+// on clicking *any* p tag, change the font of that one p tag to comic sans
+// use this for the font family: "Comic Sans MS", cursive, sans-serif
+// HINT: you might need to loop over all the p tags somehow...
