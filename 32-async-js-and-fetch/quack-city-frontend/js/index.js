41123fa53a92f116f5bc406d553ca6165e14e643
ihollander <ianholla@gmail.com>
2020-02-21 07:56:54 -0500

    before fetch

--- 32-async-js-and-fetch/quack-city-frontend/js/index.js
+++ 32-async-js-and-fetch/quack-city-frontend/js/index.js
@@ -4,6 +4,7 @@ const duckBtn = document.querySelector("#new-duck-button")
 const closeModalSpan = document.querySelector("#close-modal")
 const modalDiv = document.querySelector("#modal")
 const newDuckForm = document.querySelector("#new-duck-form")
+const detailDiv = document.querySelector("#detail")
 
 /****** Event Listeners ******/
 closeModalSpan.addEventListener("click", function () {
@@ -15,18 +16,20 @@ duckBtn.addEventListener("click", function () {
 })
 
 newDuckForm.addEventListener("submit", function (event) {
-  event.preventDefault()
+  event.preventDefault() // stop the form from making a network request and refreshing the page
+
   const newDuckObj = {
-    name: event.target.name.value,
-    imageUrl: event.target.image_url.value
+    name: event.target.name.value, // get the input field values
+    imageUrl: event.target.image_url.value,
+    rating: 0
   }
 
-  renderDuckListItem(newDuckObj)
+  renderDuckListItem(newDuckObj) // render to the list
 
-  // close the modal
-  modalDiv.classList.remove("open")
+  modalDiv.classList.remove("open") // close the modal
 })
 
+// EVENT DELEGATION
 duckListUl.addEventListener("click", event => {
   if (event.target.className === "name") {
     const duckId = parseInt(event.target.dataset.id)
@@ -42,15 +45,14 @@ duckListUl.addEventListener("click", event => {
 
 /****** Render Helpers ******/
 function renderDuckDetail(duckObj) {
-  const detailDiv = document.querySelector("#detail")
   detailDiv.innerHTML = `
     <h1>${duckObj.name}</h1>
     <div class="image">
       <h2 class="rating">${duckObj.rating}</h2>
       <img src="${duckObj.imageUrl}">
     </div>
-    <h2>Rate That Duck</h2>
-    <form id="score-form" data-id="7">
+    <h2 id="duck-rating">Rate That Duck</h2>
+    <form id="score-form" data-id="${duckObj.id}">
       <input type="number" name="score" value="${duckObj.rating}" min="0" max="10" step="1">
       <input type="submit" value="Rate">
     </form>
@@ -61,16 +63,14 @@ function renderDuckListItem(duckObj) {
   const duckLi = document.createElement("li")
   duckLi.classList.add("item")
   duckLi.innerHTML = `
-     <span class="name">${duckObj.name}</span>
-     <button class="delete">X</button>
+     <span data-id="${duckObj.id}" class="name">${duckObj.name}</span>
+     <button data-id="${duckObj.id}" class="delete">X</button>
   `
   duckListUl.append(duckLi)
 }
 
 function renderDuckList(ducksArray) {
-  ducksArray.forEach(function (duckObj) {
-    renderDuckListItem(duckObj)
-  })
+  ducksArray.forEach(duckObj => renderDuckListItem(duckObj))
 }
 
 /****** Initial Render ******/
