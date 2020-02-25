4569942c5ad42b9609f193b31a32f84366d76370
Ian Hollander <ianholla@gmail.com>
2020-02-25 11:22:09 -0500

    after debugging workshop

--- 34-debugging-workshop/index.js
+++ 34-debugging-workshop/index.js
@@ -1,27 +1,47 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
-  const jokeList = document.getElementById('joke-list')
-  const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
   let joke;
 
-  function fetchJoke(){
-    fetch('https://icanhazdadjoke.com/', {
+  // console.log({
+  //   form: form,
+  //   jokeList: jokeList,
+  //   newJokeLi: newJokeLi
+  // })
+
+  function fetchJoke() {
+    return fetch('https://icanhazdadjoke.com/', {
       headers: {
         "Accept": "application/json"
       }
     })
-    .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
+      .then(res => res.json())
+    // .then(jokeData => joke = jokeData.joke)
   }
 
   form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    // check this, what do we need an empty string for?
+    const username = document.getElementById('name-input').value
+    const username = form.username.value
 
-    if(username === "") return;
-    fetchJoke()
-    newJokeLi.innerHTML = `
-    <span class="username">${username} says:</span> ${joke}
-    `
-    jokeList.appendChild(newJokeLi)
+    const jokeList = document.getElementById('joke-list')
+
+    // check this later, why is it empty?
+    const newJokeLi = document.createElement('li')
+
+    // we need the input where the name's being typed
+    if (username === "") return;
+    const returnOfFetch = fetchJoke()
+    console.log(returnOfFetch)
+
+    returnOfFetch.then(() => {
+      // we need to create the element we want to add to the DOM
+      newJokeLi.innerHTML = `
+      <span class="username">${username} says:</span> ${joke}
+      `
+      // we need to append that element to some parent element 
+      jokeList.appendChild(newJokeLi)
+    })
+
   })
 })
