a83675e9941aad7c55036f249f8b191aa3529021
Ian Hollander <ianholla@gmail.com>
2020-03-02 10:47:14 -0500

    this

--- 38-this/index.js
+++ 38-this/index.js
@@ -2,7 +2,71 @@ function whatsThis() {
   console.log("THIS IS:", this)
 }
 
-const dog = {
-  firstName: 'fezzik',
-  favSnacks: ['peanut butter', 'soccer balls']
+function speak(greeting) {
+  console.log(`${greeting} my name is ${this.firstName}`)
 }
+
+// function sayFavSnax() {
+//   console.log("OUTSIDE forEach, this is: ", this)
+//   this.favSnacks.forEach(
+//     (snack) => {
+//       console.log("INSIDE forEach, this is: ", this)
+
+//       this.favSnacks.forEach(function () {
+//         console.log("INSIDE INSIDE forEach, this is: ", this)
+//       })
+//     }
+//   )
+//   // this.favSnacks.forEach(
+//   //   function (snack) {
+//   //     console.log("INSIDE forEach, this is: ", this)
+//   //     // console.log(`${this.firstName} likes ${snack}`)
+//   //   }.bind(this)
+//   // )
+// }
+
+// const dog = {
+//   name: 'fezzik',
+//   favSnacks: ['peanut butter', 'soccer balls'],
+//   speak: speak,
+//   // sayFavSnax: sayFavSnax
+// }
+
+// const dog2 = {
+//   firstName: 'spinny',
+//   favSnacks: ['spinach', 'tennis balls'],
+//   speak: speak,
+//   // sayFavSnax: sayFavSnax
+// }
+
+// PREHISTORIC OOJS
+// factory function
+function createDog(firstName, favSnacks) {
+  return {
+    firstName: firstName,
+    favSnacks: favSnacks,
+    speak: speak
+  }
+}
+// const dog = createDog("fezzik", ['peanut butter', 'soccer balls'])
+// const dog2 = createDog("spinny", ['spinach', 'tennis balls'])
+
+// Bronze Age OOJS
+// function Dog(firstName, favSnacks) {
+//   this.firstName = firstName
+//   this.favSnacks = favSnacks
+//   this.speak = speak
+// }
+
+
+// Modern OOJS
+class Dog {
+  constructor(firstName, favSnacks) {
+    this.firstName = firstName
+    this.favSnacks = favSnacks
+    this.speak = speak
+  }
+}
+
+const dog = new Dog("fezzik", ['peanut butter', 'soccer balls'])
+const dog2 = new Dog("spinny", ['spinach', 'tennis balls'])
